ab179ec2eb465b4e0653521923d95d777e124a9a
Zenedith <github@zenedith.mailgun.org>
2015-08-01 21:57:06 +0200

    Added scripts and run log

--- bin/run.sh
+++ bin/run.sh
@@ -22,4 +22,4 @@ cat $VCL_FILE
 
 # Starts the varnish server
 echo "Running varnishd -a $LISTEN_ADDR:$LISTEN_PORT -T $TELNET_ADDR:$TELNET_PORT -f $VCL_FILE -s file,/var/cache/varnish.cache,$CACHE_SIZE -F"
-varnishd -a $LISTEN_ADDR:$LISTEN_PORT -T $TELNET_ADDR:$TELNET_PORT -f $VCL_FILE -s file,/var/cache/varnish.cache,$CACHE_SIZE -F
+varnishd -a "$LISTEN_ADDR:$LISTEN_PORT" -T "$TELNET_ADDR:$TELNET_PORT" -f "$VCL_FILE" -s "file,/var/cache/varnish.cache,$CACHE_SIZE" -F
